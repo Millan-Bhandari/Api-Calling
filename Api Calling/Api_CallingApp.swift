539f8c04801d23d02ca8e2a29bc506ce18f51b19
Millan Bhandari <millan.ib@icloud.com>
2022-07-26 13:33:23 -0500

    Initial Commit

---
+++ Api Calling/Api_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_CallingApp.swift
+//  Api Calling
+//
+//  Created by Millan Bhandari on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Api_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
